03efa64a0afbacddf830d49e80fc08cfe88aeb13
Richard Joseph <josephri@oregonstate.edu>
2019-04-22 17:24:31 -0400

    Initial Commit

---
+++ ZooTycoon/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ZooTycoon
+//
+//  Created by Richard Joseph on 4/22/19.
+//  Copyright © 2019 Richard Joseph. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
